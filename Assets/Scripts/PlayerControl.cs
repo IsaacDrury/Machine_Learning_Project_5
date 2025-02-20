0b0d88c346a7845ffde517970922f5a4dff3632c
IsaacDrury <117859115+IsaacDrury@users.noreply.github.com>
2025-02-20 17:13:25 -0500

    Added player input
    Also made the sphere a prefab. Edited both scenes.

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,30 @@
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Vector2 moveVector;
+    private Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        moveVector = Vector2.zero;
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(moveVector.x * speed, 0, moveVector.y * speed, ForceMode.VelocityChange);
+    }
+
+    //Gets player input
+    private void OnMove(InputValue val)
+    {
+        moveVector = val.Get<Vector2>().normalized;
+    }
+}
